f99abe715dd4083a6477229dac260173e0a450f0
Cristhyan D. Marchena <demarchenac@uninorte.edu.co>
2022-10-27 03:09:27 -0500

    Chore: Add eslint, prettier pre-commit hooks

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-const express = require("express");
+import express from 'express';
+
 const app = express();
+
 const port = process.env.PORT || 3000;
 
-app.get(["/", "/:name"], (req, res) => {
-  greeting = "<h1>Hello From Node on Fly!</h1>";
-  name = req.params["name"];
-  if (name) {
-    res.send(greeting + "</br>and hello to " + name);
-  } else {
-    res.send(greeting);
-  }
+app.get(['/', '/:name'], (req, res) => {
+	const greeting = '<h1>Hello From Node on Fly!</h1>';
+	const name = req.params['name'];
+	if (name) {
+		res.send(greeting + '</br>and hello to ' + name);
+	} else {
+		res.send(greeting);
+	}
 });
 
+// eslint-disable-next-line no-console
 app.listen(port, () => console.log(`InstaYa app listening on port ${port}!`));
