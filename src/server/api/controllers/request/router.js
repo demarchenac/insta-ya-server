dd13afe08e2629c7adda32db501533203905f594
Cristhyan D. Marchena <demarchenac@uninorte.edu.co>
2022-11-12 05:13:31 -0500

    Feat: Edit is complete!

--- src/server/api/controllers/request/router.js
+++ src/server/api/controllers/request/router.js
@@ -1,8 +1,17 @@
 import express from 'express';
-import { findRequestsByUser, saveRequest } from '@models/Request/queries';
+import {
+	findByService,
+	findRequestsByUser,
+	saveRequest,
+	updateRequestByService,
+} from '@models/Request/queries';
 import { logger, validate } from '../../middlewares';
 import { hasSession } from '../../middlewares/authorization';
-import { newRequestSchema } from './schemas';
+import {
+	newRequestSchema,
+	serviceQuerySchema,
+	updateRequestSchema,
+} from './schemas';
 
 const router = express.Router();
 
@@ -20,6 +29,24 @@ router.get('/find-all', hasSession, async (req, res) => {
 	}
 });
 
+router.get(
+	'/find-by/service/:service',
+	hasSession,
+	validate(serviceQuerySchema),
+	async ({ params }, res) => {
+		try {
+			const { service } = params;
+			const query = await findByService(service);
+
+			res.status(200).json({ payload: query, message: 'Request fetched' });
+		} catch (error) {
+			// eslint-disable-next-line no-console
+			console.error(error);
+			res.status(500).json(error.message);
+		}
+	},
+);
+
 router.post(
 	'/',
 	hasSession,
@@ -29,6 +56,26 @@ router.post(
 			const payload = await saveRequest(session_payload.id, body);
 
 			res.status(200).json({ payload, message: 'Request saved!' });
+		} catch (error) {
+			// eslint-disable-next-line no-console
+			console.error(error);
+			res.status(500).json(error.message);
+		}
+	},
+);
+
+router.put(
+	'/update-by/service/:service',
+	hasSession,
+	validate(updateRequestSchema),
+	async ({ body, params }, res) => {
+		try {
+			const { service } = params;
+			// eslint-disable-next-line no-console
+			console.log({ service, body });
+			const payload = await updateRequestByService(service, body);
+
+			res.status(200).json({ payload, message: 'Request updated!' });
 		} catch (error) {
 			// eslint-disable-next-line no-console
 			console.error(error);
--- src/server/api/controllers/request/schemas.js
+++ src/server/api/controllers/request/schemas.js
@@ -8,39 +8,59 @@ const sharedNumberSchema = z
 		message: 'Must be greater than 0',
 	});
 
-export const newRequestSchema = z.object({
-	body: z.object({
-		isFragile: z.string().regex(/(true|false)/, { message: 'Not a boolean' }),
-		width: sharedNumberSchema,
-		height: sharedNumberSchema,
-		depth: sharedNumberSchema,
-		weight: sharedNumberSchema,
-		due: z
-			.string()
-			.regex(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z$/, {
-				message: 'Not a date',
-			})
-			.refine(
-				(dueStr) => {
-					const min = new Date(Date.now() + 24 * 60 * 60 * 1000); // plus 24h
-					const due = new Date(dueStr);
+const requestParams = z.object({ service: sharedNumberSchema });
 
-					return due.getTime() >= min.getTime();
-				},
-				{ message: 'Too early!' },
-			),
-		fromCity: z.string().min(3).max(50),
-		fromAddress: z.string().min(3).max(100),
-		toCity: z.string().min(3).max(50),
-		toAddress: z.string().min(3).max(100),
-		toOwner: z.string().min(3).max(50),
-		toOwnerId: z
-			.string()
-			.min(3)
-			.max(11)
-			.refine((num) => !isNaN(num), { message: 'Not a number' })
-			.refine((num) => !isNaN(num) && parseInt(num) > 99, {
-				message: 'Minimum value is 100',
-			}),
+export const serviceQuerySchema = z.object({ params: requestParams });
+
+const sharedRequestBody = {
+	isFragile: z.string().regex(/(true|false)/, { message: 'Not a boolean' }),
+	width: sharedNumberSchema,
+	height: sharedNumberSchema,
+	depth: sharedNumberSchema,
+	weight: sharedNumberSchema,
+	fromCity: z.string().min(3).max(50),
+	fromAddress: z.string().min(3).max(100),
+	toCity: z.string().min(3).max(50),
+	toAddress: z.string().min(3).max(100),
+	toOwner: z.string().min(3).max(50),
+	toOwnerId: z
+		.string()
+		.min(3)
+		.max(11)
+		.refine((num) => !isNaN(num), { message: 'Not a number' })
+		.refine((num) => !isNaN(num) && parseInt(num) > 99, {
+			message: 'Minimum value is 100',
+		}),
+};
+
+const requestBody = z.object({
+	...sharedRequestBody,
+	due: z
+		.string()
+		.regex(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z$/, {
+			message: 'Not a date',
+		})
+		.refine(
+			(dueStr) => {
+				const min = new Date(Date.now() + 24 * 60 * 60 * 1000); // plus 24h
+				const due = new Date(dueStr);
+
+				return due.getTime() >= min.getTime();
+			},
+			{ message: 'Too early!' },
+		),
+});
+
+const updateBody = z.object({
+	...sharedRequestBody,
+	due: z.string().regex(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z$/, {
+		message: 'Not a date',
 	}),
+});
+
+export const newRequestSchema = z.object({ body: requestBody });
+
+export const updateRequestSchema = z.object({
+	params: requestParams,
+	body: updateBody,
 });
